70d5921369717bea48ecf0df9dbd6ccc1c8f3264
DEE DEDS <mtfilmes05@gmail.com>
2022-02-04 23:55:10 +0000

    .

--- site/pages/index.vue
+++ site/pages/index.vue
@@ -1,8 +1,26 @@
 <template>
+<div>
+    <div class="bg-indigo-600">
+    <div class="max-w-7xl mx-auto py-1 px-3 sm:px-7 lg:px-7">
+      <div class="flex items-center justify-between flex-wrap">
+        <div class="w-0 flex-1 flex items-center">
+          <p class="ml-3 font-medium text-white truncate">
+            <span class="md:hidden">
+              Site e bot possivelmente instavel
+            </span>
+            <span class="align-center hidden md:inline">
+             Estamos em atualizações frequentes, oque pode causar instabilidade no site 
+             ou no bot e poderá resultar em bugs contate o suporte caso ache um !
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 
  <nav class="flex items-center justify-between flex-wrap  p-6">
-  <div class="flex items-center flex-shrink-0 text-white mr-6">
-    <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+  <div class="flex items-center flex-shrink-0 text-white mr-6 pdg10">
+    <svg class=" fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
     <span class="font-semibold text-xl tracking-tight">Back+</span>
   </div>
   <div class="block lg:hidden">
@@ -12,13 +30,13 @@
   </div>
   <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
     <div class="text-sm lg:flex-grow">
-      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-slate-300 mr-4">
         Comandos
       </a>
-      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-slate-300 mr-4">
         Sobre
       </a>
-      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-slate-300">
         Termos
       </a>
     </div>
@@ -29,9 +47,30 @@
  
 </nav>
 
+<div class="txtps">
+<p class="text-2xl app ...">Conheça nosso bot feito em 2022
+  <br>este bot foi feito para ajudar controlar seu servidor
+  <br>e divertir-lo ao mesmo tempo
+  </p>
+</div>
+<div class="txtos">
+  <p class="text-1xl app opacity-75 ...">Versão: Alpha 0.0.1</p>
+</div>
+
+
+<div class="div2">
+  <p>teste</p>
+</div>
+
+
+</div>
+
+
 </template>
 
+<script>
 
+</script>
 <script>
 export default {
   name: 'IndexPage'
@@ -46,10 +85,31 @@ deveria entrar na nossa equipe nos precisamos de você
 entre no grupo oficial com o codigo secreto dos hackers
 "208552410230" vamos enteder que você quer entrar pra equipe
 iremos testar sua capacidade :)`); 
+
+
 </script>
 
 <style>
+
 body {
   background-color: #202124
+}
+
+.txtps {
+   padding-top: 200px;
+   padding-left: 40px;
+   color: white
+}
+.txtos {
+     padding-left: 40px;
+     color: white
+}
+
+.pdg10 {
+    padding-left: 40px;
+}
+
+.div2 {
+  padding-top: 500px;
 }
 </style>
