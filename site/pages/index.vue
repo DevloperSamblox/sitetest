2aa0d9f9c9dab4beb2259efebaefdc8b12441c4a
DEE DEDS <mtfilmes05@gmail.com>
2022-02-07 01:05:12 +0000

    .

--- site/pages/index.vue
+++ site/pages/index.vue
@@ -1,5 +1,7 @@
 <template>
 <div>
+  
+
     <div class="bg-indigo-600">
     <div class="max-w-7xl mx-auto py-1 px-3 sm:px-7 lg:px-7">
       <div class="flex items-center justify-between flex-wrap">
@@ -8,6 +10,7 @@
             <span class="md:hidden">
               Site e bot possivelmente instavel
             </span>
+            
             <span class="align-center hidden md:inline">
              Estamos em atualizações frequentes, oque pode causar instabilidade no site 
              ou no bot e poderá resultar em bugs contate o suporte caso ache um !
@@ -18,34 +21,21 @@
     </div>
   </div>
 
- <nav class="flex items-center justify-between flex-wrap  p-6">
-  <div class="flex items-center flex-shrink-0 text-white mr-6 pdg10">
-    <svg class=" fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+ <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+  <div class="flex items-center flex-shrink-0 text-white mr-6">
+    <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
     <span class="font-semibold text-xl tracking-tight">Back+</span>
   </div>
-  <div class="block lg:hidden">
-    <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-      <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-    </button>
-  </div>
-  <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+
+ <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
     <div class="text-sm lg:flex-grow">
-      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-slate-300 mr-4">
-        Comandos
-      </a>
-      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-slate-300 mr-4">
-        Sobre
-      </a>
-      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-slate-300">
-        Termos
-      </a>
     </div>
     <div>
-      <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-100 hover:bg-slate-500 mt-4 lg:mt-0">Adicione</a>
+      <a href="#" class="hidden md:inline inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Adicionar</a>
     </div>
   </div>
- 
 </nav>
+
 
 <div class="txtps">
 <p class="text-2xl app ...">Conheça nosso bot feito em 2022
@@ -53,18 +43,17 @@
   <br>e divertir-lo ao mesmo tempo
   </p>
 </div>
-<div class="txtos">
-  <p class="text-1xl app opacity-75 ...">Versão: Alpha 0.0.1</p>
+    <div>
+      <a href="#" class="md:hidden inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Adicionar</a>
+    </div>
+
+
+
+
+
+
 </div>
 
-
-
-
-
-
-
-
-</div>
 
 
 
@@ -107,9 +96,7 @@ body {
      color: white
 }
 
-.pdg10 {
-    padding-left: 40px;
-}
+
 
 .div2 {
   padding-top: 500px;
